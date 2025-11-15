0b2db828f3c05bda0a0bbc1b4be1da7997d613a7
Killian Duroux <kduroux@bess-f2r5s5.clusters.42paris.fr>
2025-11-15 20:28:59 +0100

    Hellp world

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,7 +1,24 @@
 ; on definie start comme variable global a tous le systeme
 ; hello world test
-global start
+section .data
+msg: DB 'Hello World!', 10
+msgSize EQU $ - msg
+
+global _start
 
 section .text
-    start:
-    ret
+
+_start:
+    ; write(1, msg,msgSize)
+    mov rax, 1          ; syscall number: sys_write (1 en 64-bit)
+    mov rdi, 1          ; arg1: fd = stdout
+    mov rsi, msg        ; arg2: buffer
+    mov rdx, msgSize    ; arg3: count
+    syscall
+    
+    ; exit(0)
+    mov rax, 60         ; syscall number: sys_exit (60 en 64-bit)
+    mov rdi, 0          ; arg1: status = 0
+    syscall             ; int 0x80 -> 32 bit | syscall -> 64 bit 
+    ; PAS de ret ici ! Le programme se termine avec exit
+    ; ret ; ret = return mais quand on exit et que ret est appele alors il return a un endroit quil  ne conais pas 
